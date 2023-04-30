54328e291145243e12c783f4109390d9c70d1cc8
Shyrshik <mr.gitarast@mail.ru>
2023-04-30 14:19:30 +0600

    Добавил тайлмапы, все поломал - потому что было жеско завязано на каких то спрайтах. Теперь их не может ничего обнаружить)

---
+++ Assets/Documentation.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Documentation
+{
+    /*
+     *  Слои (Order in Layer):
+     *      UI                      2000;
+     *      FrontGround             1500;
+     *      Player                  1000;
+     *      Ground                  1000;
+     *      Enemies                 900;
+     *      BackGround              500;
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     */
+}
