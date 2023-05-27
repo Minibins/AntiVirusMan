60759878975c1cb3733ab530e8a32f939dacb79c
Minibins <mini6ins@gmail.com>
2023-05-27 11:59:04 +0200

    first

--- Assets/Scripts/Persons/Move.cs
+++ Assets/Scripts/Persons/Move.cs
@@ -1,16 +1,32 @@
 using System;
 using UnityEngine;
+using System.Collections.Generic;
 [RequireComponent(typeof(Rigidbody2D)),
     RequireComponent(typeof(Animator)),
     RequireComponent(typeof(SpriteRenderer))]
 public class Move : MonoBehaviour
 {
-    /// <summary>
-    /// True - передвигается только влево или вправо, может прыгать, на тело действует гравитация.
-    /// False - Летает по экрано во всех направлениях, гравитация отключена.
-    /// </summary>
+    [SerializeField] private PathTypes PathType;
+    [SerializeField] private float maxDistance = .1f;
     [SerializeField] private bool _canJump;
-    public bool CanJump
+    [SerializeField] private float _speed = 1f;
+    [SerializeField] private float _jumpingPower = 10f;
+    private Way way;
+    private IEnumerator<Transform> pointInPath;
+    private float _speedMultiplier = 1f;
+    private float _curentSpeed;
+    private Action _move;
+    private Rigidbody2D _rigidbody2D;
+    private Animator _animator;
+    private SpriteRenderer _spriteRenderer;
+
+    private enum PathTypes
+    {
+        Wallking,
+        WallkingOnWire,
+    }
+
+    private Vector2 _velocity = Vector2.zero; public bool CanJump
     {
         get
         {
@@ -31,35 +47,49 @@ public class Move : MonoBehaviour
             }
         }
     }
-    [SerializeField] private float _speed = 1f;
-    [SerializeField] private float _jumpingPower = 10f;
-    private float _speedMultiplier = 1f;
-    private float _curentSpeed;
-    private Action _move;
-    private Rigidbody2D _rigidbody2D;
-    private Animator _animator;
-    private SpriteRenderer _spriteRenderer;
-    private Vector2 _velocity = Vector2.zero;
+
+    private void Awake()
+    {
+        way = GameObject.FindGameObjectWithTag("Way").GetComponent<Way>();
+        pointInPath = way.GetNextPathPoint();
+        pointInPath.MoveNext();
+        _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        SetSpeedMultiplierForOllTime(_speedMultiplier);
+    }
+
+    private void FixedUpdate()
+    {
+        _move();
+        EnemyMoves();
+    }
+
+
     public void MoveHorizontally(float direction)
     {
-        _velocity.Set(0f, _rigidbody2D.velocity.y);
-        if (direction == 0f)
+        if (PathType == PathTypes.Wallking)
         {
-            _animator.SetBool("IsRunning", false);
-        }
-        else if (direction < 0f)
-        {
-            _velocity.x = -_curentSpeed;
-            _spriteRenderer.flipX = true;
-            _animator.SetBool("IsRunning", true);
-        }
-        else
-        {
-            _velocity.x = _curentSpeed;
-            _spriteRenderer.flipX = false;
-            _animator.SetBool("IsRunning", true);
+            _velocity.Set(0f, _rigidbody2D.velocity.y);
+            if (direction == 0f)
+            {
+                _animator.SetBool("IsRunning", false);
+            }
+            else if (direction < 0f)
+            {
+                _velocity.x = -_curentSpeed;
+                _spriteRenderer.flipX = true;
+                _animator.SetBool("IsRunning", true);
+            }
+            else
+            {
+                _velocity.x = _curentSpeed;
+                _spriteRenderer.flipX = false;
+                _animator.SetBool("IsRunning", true);
+            }
         }
     }
+
     public void MoveVertically(float direction)
     {
         _velocity.Set(_rigidbody2D.velocity.x, 0f);
@@ -120,21 +150,12 @@ public class Move : MonoBehaviour
             MoveVertically(_velocity.y);
         }
     }
-    private void Awake()
-    {
-        _animator = GetComponent<Animator>();
-        _spriteRenderer = GetComponent<SpriteRenderer>();
-        _rigidbody2D = GetComponent<Rigidbody2D>();
-        SetSpeedMultiplierForOllTime(_speedMultiplier);
-    }
+
     private void OnEnable()
     {
         CanJump = CanJump;
     }
-    private void FixedUpdate()
-    {
-        _move();
-    }
+
     private void MoveAndJump()
     {
         _velocity.y = _rigidbody2D.velocity.y;
@@ -147,5 +168,26 @@ public class Move : MonoBehaviour
     private void OnDisable()
     {
         _move = null;
+    }
+
+
+    private void EnemyMoves()
+    {
+        if (PathType == PathTypes.WallkingOnWire)
+        {
+            _rigidbody2D.gravityScale = 0;
+            if (pointInPath == null || pointInPath.Current == null)
+            {
+                return;
+            }
+
+            transform.position = Vector3.MoveTowards(transform.position, pointInPath.Current.position, Time.deltaTime * _speed);
+
+            var distanceSqure = (transform.position - pointInPath.Current.position).sqrMagnitude;
+            if (distanceSqure < maxDistance * maxDistance)
+            {
+                pointInPath.MoveNext();
+            }
+        }
     }
 }
