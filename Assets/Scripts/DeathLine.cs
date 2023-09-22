1fea3560ae04d682171359421425d5a52b1a2b05
Minibins <127485465+Minibins@users.noreply.github.com>
2023-09-22 21:04:59 +0200

    3

--- Assets/Scripts/DeathLine.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class DeathLine : MonoBehaviour
-{
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        Destroy(collision.gameObject);
-    }
-}
--- Assets/Scripts/Saver/Data.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.UI;
-
-[System.Serializable]
-public class Data
-{
-    public float MusicVolume;
-    public int WinLocation;
-}
