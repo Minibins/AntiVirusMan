9b9e572242267dcf8b12328195e744eb456ca575
AntonXCM <antony.prykhodchenko@gmail.com>
2023-10-30 14:33:08 +0300

    Исправлен абуз переноса компьютера

--- Assets/Scripts/Persons/pc.cs
+++ Assets/Scripts/Persons/pc.cs
@@ -1,23 +1,61 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PC : MonoBehaviour
 {
-    [SerializeField] Animator animator;
-
-    
+    [SerializeField] private int radius;
+    private Health health;
+    private Animator animator;
+    private Transform rozetka;
+    private Animator rozetkaAnim;
+    private bool lowchrge;
+    private void Start()
+    {
+        health = GetComponentInParent<Health>();
+        animator = GetComponentInParent<Animator>();
+        rozetka = GameObject.Find("Rozetka").transform;
+        rozetkaAnim=rozetka.GetComponent<Animator>();
+        StartCoroutine(LowCharge());
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag=="Enemy")
         {
             animator.SetTrigger("NoCalm");
         }
+        
     }
     public void EnemyKilled()
     {
+            animator.SetTrigger("HeKilledEnemy");    
+    }
+    private void FixedUpdate()
+    {
+        if(Vector2.Distance(rozetka.position,transform.position) > radius)
+        {
+            rozetkaAnim.SetBool("Sad",true);
+            lowchrge = true;
+        }
+        else
+        {
+            rozetkaAnim.SetBool("Sad",false);
+            lowchrge = false;
+        }
+    }
+    IEnumerator LowCharge()
+    {
+        while(true)
+        {
+        yield return new WaitForSeconds(5);
+        while(lowchrge) {
+            health.ApplyDamage(1);
+            yield return new WaitForSeconds(5);
+        }
+        }
         
-            animator.SetTrigger("HeKilledEnemy");
         
     }
 }
