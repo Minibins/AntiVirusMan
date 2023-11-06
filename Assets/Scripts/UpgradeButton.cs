6f43ae16d1027f2d2253bbdc936d4baeb6494055
Minibins <127485465+Minibins@users.noreply.github.com>
2023-11-06 16:54:04 +0100

    Доделал "Большой удар"

--- Assets/Scripts/UpgradeButton.cs
+++ Assets/Scripts/UpgradeButton.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using Unity.VisualScripting;
-
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -59,13 +58,13 @@ public class UpgradeButton : MonoBehaviour
     //Works
     [SerializeField] private GameObject DashButton;
     //ID 10
-    
+
     //ID 11
     //ID 11
-    
+
     //ID 12
     //ID 12
-    
+
     //ID 13
     [SerializeField] private GameObject _backStager;
     //ID 13
@@ -86,9 +85,9 @@ public class UpgradeButton : MonoBehaviour
             LevelUP.IsSelected = true;
         }
     }
+
     private void ChooseUpgrade(LevelUP lvlUp)
     {
-
         switch (id)
         {
             case 0:
@@ -98,46 +97,56 @@ public class UpgradeButton : MonoBehaviour
                 {
                     Laser.AddComponent<DRAG>();
                 }
+
                 break;
+
             case 1:
+
                 GameObject.FindGameObjectWithTag("LaserGun").AddComponent<DRAG>();
                 GameObject.FindGameObjectWithTag("PC").AddComponent<DRAG>();
                 GameObject.FindGameObjectWithTag("PUSHKA").AddComponent<DRAG>();
                 _Player.GetComponent<InstantiateWall>().canmove = true;
                 break;
+
             case 2:
                 GameObject[] starfallObjects = GameObject.FindGameObjectsWithTag("Starfall");
                 foreach (GameObject starfallObject in starfallObjects)
                 {
                     starfall starfallComponent = starfallObject.GetComponent<starfall>();
-                    if (starfallComponent != null)  starfallComponent.IsSpawn = true;
+                    if (starfallComponent != null) starfallComponent.IsSpawn = true;
                 }
+
                 break;
+
             case 3:
                 _Player.GetComponent<InstantiateWall>().IsOpenly = true;
                 break;
+
             case 4:
                 DownButton.SetActive(true);
                 break;
+
             case 5:
                 _Player.GetComponent<PlayerAttack>().IsSelectedBullet = true;
                 break;
+
             case 6:
                 PUSHKA = GameObject.FindGameObjectWithTag("PUSHKA");
-            SpriteRenderer[] colesa= PUSHKA.GetComponentsInChildren<SpriteRenderer>();
-            for (int i = 0; i < colesa.Length; i++)
-            {
-                colesa[i].enabled = true;
-            }
+                SpriteRenderer[] colesa = PUSHKA.GetComponentsInChildren<SpriteRenderer>();
+                for (int i = 0; i < colesa.Length; i++)
+                {
+                    colesa[i].enabled = true;
+                }
+
                 PUSHKA.GetComponent<PUSHKA>().StartShoot();
                 if (lvlUp.IssTake[1])
                 {
-                for(int i = 0; i < colesa.Length; i++)
-                {
-                    colesa[i].AddComponent<DRAG>();
+                    for (int i = 0; i < colesa.Length; i++)
+                    {
+                        colesa[i].AddComponent<DRAG>();
+                    }
                 }
-               
-                }
+
                 break;
 
             case 7:
@@ -146,53 +155,65 @@ public class UpgradeButton : MonoBehaviour
                 break;
 
             case 8:
-                GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerAttack>().isSpeedIsDamage = true;
+                _Player.GetComponent<PlayerAttack>().isSpeedIsDamage = true;
                 break;
+
 
             case 9:
                 GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>().StartSpawnPortals();
                 break;
+
             case 10:
                 DashButton.SetActive(true);
                 break;
+
             case 11:
                 SpawnerEnemy.elitePossibility[0] = true;
                 break;
+
             case 12:
                 GameObject[] AuraObjects = GameObject.FindGameObjectsWithTag("Aura");
                 foreach (GameObject AuraObject in AuraObjects)
                 {
                     AuraPC AuraComponent = AuraObject.GetComponent<AuraPC>();
-                    if (AuraComponent != null)  AuraComponent.IsStartWork = true;
+                    if (AuraComponent != null) AuraComponent.IsStartWork = true;
                 }
+
                 break;
+
             case 13:
-            Health.backStager = true;
+                Health.backStager = true;
                 break;
+
             case 14:
                 Enemy.isDraggable = true;
             {
-                GameObject[] enemies=GameObject.FindGameObjectsWithTag("Enemy");
-                foreach(GameObject enemy in enemies)
+                GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+                foreach (GameObject enemy in enemies)
                 {
                     enemy.AddComponent<DRAG>();
                 }
             }
                 break;
+
             case 15:
                 Player.isFlying = true;
                 break;
+
             case 16:
                 Enemy.AntivirusHaveBoots = true;
             {
-GameObject[] enemies=GameObject.FindGameObjectsWithTag("Enemy");
-            foreach(GameObject enemy in enemies)
-            {
+                GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+                foreach (GameObject enemy in enemies)
+                {
                     enemy.GetComponent<Enemy>().AddBookaComponent();
+                }
             }
-            }
+                break;
             
-            break;
+            case 17:
+                _Player.GetComponent<PlayerAttack>().IsUltraAttack = true;
+                break;
         }
     }
 }
