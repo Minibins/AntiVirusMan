89a9cfd06f1c4e620a796178b8bb5ec2210b08e8
AntonXCM <antony.prykhodchenko@gmail.com>
2023-11-07 23:55:09 +0300

    Починил тоску

--- Assets/Scripts/Upgrade/DRAG.cs
+++ Assets/Scripts/Upgrade/DRAG.cs
@@ -76,41 +76,6 @@ public class DRAG : MonoBehaviour,Draggable
 
     }
 
-    private void FixedUpdate()
-    {
-        if (isdrgging) { rb.freezeRotation = false;
-            
-
-        }
-        else { rb.freezeRotation = true; }
-        if (Input.touchCount > 0 && pa.Ammo > 0)
-        {
-            Touch touch = Input.GetTouch(0);
-            if(Physics2D.Raycast( Camera.main.ScreenToWorldPoint(touch.position),Vector2.down))
-            {
-                switch(touch.phase)
-                {
-                    case TouchPhase.Began:
-                    MyScript.OnDrag();
-                    isdrgging = true;
-                    StaminaConchaeca();
-                    offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x,Input.mousePosition.y));
-                    break;
-
-                    case TouchPhase.Moved:
-                    Vector2 curPosition = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) + offset;
-                    rb.velocity = (curPosition - rb.position) * 10f; ;
-                    break;
-
-                    case TouchPhase.Ended:
-                    isdrgging = false;
-                    MyScript.OnDragEnd();
-                    break;
-                }
-            }
-           
-        }
-    }
     private void StaminaConchaeca()
     {
         if (isdrgging) {pa.Ammo--;
