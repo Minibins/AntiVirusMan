3bddafea08fc90fa3274579650b9ab8e82f69c3a
Minibins <127485465+Minibins@users.noreply.github.com>
2023-11-23 08:43:27 +0100

    bug fix

---
+++ Assets/Scripts/UI/DraggButtonController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class DraggButtonController : MonoBehaviour
+{
+    [SerializeField] private DraggableButton[] _buttons;
+    [SerializeField] private GameObject[] _buttonsList;
+    public static bool Draggble;
+
+    public void SaveButtonsPosition()
+    {
+        foreach (DraggableButton button in _buttons)
+        {
+            button.ResetPosition();
+        }
+    }
+
+    public void LoadPosition()
+    {
+        foreach (var button in _buttonsList)
+        {
+            button.GetComponent<LoadButtonPosition>().LoadPosition();
+        }
+    }
+    
+    public void Dragg(bool isDragg)
+    {
+        Draggble = isDragg;
+    }
+}
