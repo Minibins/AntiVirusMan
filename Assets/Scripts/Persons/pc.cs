8bae75e194eaede64462cea9ceaa1fe19618e0c0
Minibins <127485465+Minibins@users.noreply.github.com>
2023-11-24 18:29:18 +0100

    fix

--- Assets/Scripts/Persons/pc.cs
+++ Assets/Scripts/Persons/pc.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -19,102 +18,113 @@ public class PC : Follower
     public bool OnlyBehind;
     private static float carma;
     public static bool IsFollowing;
-    public static float Carma { get => carma; set { carma = value;
-       GameObject.Find("PC").GetComponentInChildren<PC>(). UpdateCarma();
-        } }
 
-    override private protected void Start()
+    public static float Carma
+    {
+        get => carma;
+        set
+        {
+            carma = value;
+            GameObject.Find("PC").GetComponentInChildren<PC>().UpdateCarma();
+        }
+    }
+
+    private protected override void Start()
     {
         _player = GameObject.FindGameObjectWithTag("Player");
-        playerPosition=_player.transform;
+        playerPosition = _player.transform;
         health = GetComponentInParent<Health>();
         animator = GetComponentInParent<Animator>();
         rozetka = GameObject.Find("Rozetka").transform;
         rozetkaAnim = rozetka.GetComponent<Animator>();
         StartCoroutine(LowCharge());
-        rb=GetComponentInParent<Rigidbody2D>();
+        rb = GetComponentInParent<Rigidbody2D>();
         PC.Carma = 7;
         IsFollowing = false;
     }
 
     private protected void FixedUpdate()
     {
-        if(Vector2.Distance(rozetka.position,transform.position) > radius)
+        if (Vector2.Distance(rozetka.position, transform.position) > radius)
         {
-            rozetkaAnim.SetBool("Sad",true);
+            rozetkaAnim.SetBool("Sad", true);
             lowchrge = true;
         }
         else
         {
-            rozetkaAnim.SetBool("Sad",false);
+            rozetkaAnim.SetBool("Sad", false);
             lowchrge = false;
         }
-
-       
     }
-    override private protected void Update()
+
+    private protected override void Update()
     {
-        if(IsFollowing)
+        if (IsFollowing)
         {
             Following(
-        new Vector3(
-        playerPosition.position.x + (Convert.ToInt16(_player.GetComponent<SpriteRenderer>().flipX) - 0.5f) * distanceFromPlayer,
-        pc.transform.position.y,
-        0)
-        ,
-        !lowchrge,pc);
-            animator.SetBool("IsRunning",!lowchrge && (Mathf.Abs(playerPosition.position.x - pc.position.x) > distanceFromPlayer / 2) || OnlyBehind);
+                new Vector3(
+                    playerPosition.position.x + (Convert.ToInt16(_player.GetComponent<SpriteRenderer>().flipX) - 0.5f) *
+                    distanceFromPlayer,
+                    pc.transform.position.y,
+                    0)
+                ,
+                !lowchrge, pc);
+            animator.SetBool("IsRunning",
+                !lowchrge && (Mathf.Abs(playerPosition.position.x - pc.position.x) > distanceFromPlayer / 2) ||
+                OnlyBehind);
         }
     }
-    override private protected void Move(Vector3 startPos,Transform transforme)
+
+    private protected override void Move(Vector3 startPos, Transform transforme)
     {
-        if(Mathf.Abs(playerPosition.position.x - transforme.position.x) > distanceFromPlayer / 2)
+        if (Mathf.Abs(playerPosition.position.x - transforme.position.x) > distanceFromPlayer / 2)
         {
             transforme.position =
-                Vector2.MoveTowards(transforme.position,startPos,speed * Time.deltaTime);
+                Vector2.MoveTowards(transforme.position, startPos, speed * Time.deltaTime);
         }
-        else if(OnlyBehind)
+        else if (OnlyBehind)
         {
             transforme.position =
-                    Vector2.MoveTowards(transforme.position,startPos,speed * Time.deltaTime * 2);
-            if(Vector2.Distance(transforme.position,startPos) < 0.1f)
+                Vector2.MoveTowards(transforme.position, startPos, speed * Time.deltaTime * 2);
+            if (Vector2.Distance(transforme.position, startPos) < 0.1f)
             {
                 OnlyBehind = false;
             }
         }
-        
     }
+
     private void UpdateCarma()
     {
-        switch(Convert.ToInt16(Carma))
+        switch (Convert.ToInt16(Carma))
         {
             default:
                 if (Carma < 0) capmaImage.sprite = carmaSprites[0];
                 else capmaImage.sprite = carmaSprites[6];
-            break;
+                break;
             case 1:
-            capmaImage.sprite = carmaSprites[1];
-            break;
+                capmaImage.sprite = carmaSprites[1];
+                break;
             case 2:
-            capmaImage.sprite = carmaSprites[2];
-            break; 
+                capmaImage.sprite = carmaSprites[2];
+                break;
             case 3:
-            capmaImage.sprite = carmaSprites[3];
-            break;
+                capmaImage.sprite = carmaSprites[3];
+                break;
             case 4:
-            capmaImage.sprite = carmaSprites[4];
-            break; 
+                capmaImage.sprite = carmaSprites[4];
+                break;
             case 5:
-            capmaImage.sprite = carmaSprites[4];
-            break; 
+                capmaImage.sprite = carmaSprites[4];
+                break;
             case 6:
-            capmaImage.sprite = carmaSprites[5];
-            break; 
+                capmaImage.sprite = carmaSprites[5];
+                break;
             case 7:
-            capmaImage.sprite = carmaSprites[6];
-            break;
+                capmaImage.sprite = carmaSprites[6];
+                break;
         }
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Enemy")
