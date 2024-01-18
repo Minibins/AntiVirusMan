91f067807174cd92edb29c89081605bd0b231699
AntonXCM <antony.prykhodchenko@gmail.com>
2024-01-18 12:06:48 +0200

    Антивирус просит разрешение на узнавание имени

--- Assets/Scripts/DustyStudio's/GetUsername.cs
+++ Assets/Scripts/DustyStudio's/GetUsername.cs
@@ -9,38 +9,70 @@ namespace DustyStudios
         {
             public static string Username()
             {
+                if(!Permission.HasUserAuthorizedPermission("Game characters want to know your username")|| !PlayerPrefs.HasKey("KnowPlayersName"))
+                {
+                    Permission.RequestUserPermission("Game characters want to know your username");
+                }
+                else if(PlayerPrefs.GetInt("KnowPlayersName")==1)
+                {
+                    return getUsername();
+                }
+                
+                return "Antivirus";
+            }
+            static string getUsername()
+            {
                 switch(SystemInfo.deviceType)
                 {
                     case DeviceType.Desktop:
-                        if(SystemInfo.operatingSystemFamily == OperatingSystemFamily.Windows|| SystemInfo.operatingSystemFamily == OperatingSystemFamily.MacOSX)
-                        {
-                            return Environment.UserName;
-                        }
-                        else if(SystemInfo.operatingSystemFamily == OperatingSystemFamily.Linux)
-                        {
-                            return Environment.GetEnvironmentVariable("USER");
-                        }
+                    if(SystemInfo.operatingSystemFamily == OperatingSystemFamily.Windows || SystemInfo.operatingSystemFamily == OperatingSystemFamily.MacOSX)
+                    {
+                        return Environment.UserName;
+                    }
+                    else if(SystemInfo.operatingSystemFamily == OperatingSystemFamily.Linux)
+                    {
+                        return Environment.GetEnvironmentVariable("USER");
+                    }
                     break;
                     case DeviceType.Handheld:
-                        try
+                    try
+                    {
+                        AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+                        AndroidJavaObject currentActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+                        AndroidJavaObject contentResolver = currentActivity.Call<AndroidJavaObject>("getContentResolver");
+                        AndroidJavaClass secure = new AndroidJavaClass("android.provider.Settings$Secure");
+                        string userId = secure.CallStatic<string>("getString", contentResolver, "android_id");
+                        if(!string.IsNullOrEmpty(userId))
                         {
-                            AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-                            AndroidJavaObject currentActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
-                            AndroidJavaObject contentResolver = currentActivity.Call<AndroidJavaObject>("getContentResolver");
-                            AndroidJavaClass secure = new AndroidJavaClass("android.provider.Settings$Secure");
-                            string userId = secure.CallStatic<string>("getString", contentResolver, "android_id");
-                            if (!string.IsNullOrEmpty(userId)) 
-                            { 
-                                return userId;
-                            }
+                            return userId;
                         }
-                        catch
-                        {
+                    }
+                    catch
+                    {
 
-                        }
+                    }
                     break;
                 }
                 return "Antivirus";
+            }
+            private class PermissionCallbacker : PermissionCallbacks
+            {
+                public void OnPermissionGranted(string permissionName)
+                {
+                    if(permissionName.Equals("Game characters want to know your username"))
+                    {
+                        PlayerPrefs.SetInt("KnowPlayersName",1);
+                    }
+                }
+
+                public void OnPermissionDenied(string permissionName)
+                {
+                    if(permissionName.Equals("Game characters want to know your username"))
+                    {
+                        PlayerPrefs.SetInt("KnowPlayersName", 0);
+                    }
+                }
+
             }
         }
     }
