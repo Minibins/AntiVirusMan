90f68d0f7f6cff0896bf157280fc30ae975765a8
AntonXCM <antony.prykhodchenko@gmail.com>
2024-02-20 00:16:06 +0200

    немного починил генератор, но не совсем

--- Assets/Scripts/World/TV Level/Generator/Debugger.cs
+++ Assets/Scripts/World/TV Level/Generator/Debugger.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 using Unity.Mathematics;
 
 using UnityEngine;
@@ -6,19 +8,23 @@ public class Debugger : MonoBehaviour
 {
     [SerializeField] int rows, columns, edgeHeight, centerHeight;
     [SerializeField] Vector2Int stairSize;
-    [SerializeField] float cubeSize = 1.0f; 
-
+    [SerializeField] float cubeSize = 1.0f;
+    List<GameObject> gameObjects = new();
     private bool[,] boolArray;
     int AbsoluteStairSize
     {
         get => (stairSize.x + stairSize.y) / 2;
     }
+    int AbsoluteStairHeight
+    {
+        get => (edgeHeight - centerHeight) / AbsoluteStairSize;
+    }
     void Start()
     {
         boolArray = new bool[rows,columns];
-        for(int i = 0; i < rows/ AbsoluteStairSize; )
+        for(int i = 0; i < rows / AbsoluteStairSize;)
         {
-            int height = math.abs((edgeHeight-centerHeight)*((rows/2)+((i+1)*AbsoluteStairSize)));
+            int height = (int)math.abs((float)AbsoluteStairHeight * (((float)i - (float)rows/2f) ));
             for(int x = 0; x < AbsoluteStairSize;)
                 try
                 {
@@ -26,12 +32,18 @@ public class Debugger : MonoBehaviour
                 }
                 catch
                 {
-                    DustyStudios.DustyConsole.Print("X="+ (i * AbsoluteStairSize + x) + "    Y="+height);
+                    DustyStudios.DustyConsole.Print("X=" + (i * AbsoluteStairSize + x) + "    Y=" + height);
                 }
             i++;
         }
 
         CreateCubesFromBoolArray();
+    }
+    private void OnValidate()
+    {
+        foreach(GameObject obj in gameObjects) UnityEditor.EditorApplication.delayCall += () => DestroyImmediate(obj,true);
+        gameObjects.Clear();
+        Start();
     }
     void CreateCubesFromBoolArray()
     {
@@ -41,9 +53,10 @@ public class Debugger : MonoBehaviour
             {
                 if(boolArray[x,y])
                 {
-                    Vector3 cubePosition = new Vector3(x * cubeSize, y * cubeSize, 0);
+                    Vector3 cubePosition = new Vector2(x,y)*cubeSize;
                     GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
                     cube.transform.position = cubePosition;
+                    gameObjects.Add(cube);
                 }
             }
         }
