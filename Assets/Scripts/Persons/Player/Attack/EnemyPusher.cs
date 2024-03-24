83fc540652fc694651fb89409d9c0145e1e661af
AntonXCM <antony.prykhodchenko@gmail.com>
2024-03-24 11:48:36 +0200

    Стены помогают отталкиванию врагов

--- Assets/Scripts/Persons/Player/Attack/EnemyPusher.cs
+++ Assets/Scripts/Persons/Player/Attack/EnemyPusher.cs
@@ -1,5 +1,7 @@
 using DustyStudios.MathAVM;
 
+using System;
+
 using UnityEngine;
 
 public class EnemyPusher : MonoBehaviour, IAttackProjectileModule
@@ -10,7 +12,7 @@ public class EnemyPusher : MonoBehaviour, IAttackProjectileModule
         MoveBase move;
         if(collision.gameObject.TryGetComponent<MoveBase>(out move))
         {
-            move.Rigidbody.velocity = new( MathA.OneOrNegativeOne(GetComponent<SpriteRenderer>().flipX)*Power,move.Rigidbody.velocity.y);
+            move.Rigidbody.velocity = new( MathA.OneOrNegativeOne(GetComponent<SpriteRenderer>().flipX)*Power*(1+Convert.ToInt16(collision.IsTouchingLayers(4))),move.Rigidbody.velocity.y);
         }
     }
 }
