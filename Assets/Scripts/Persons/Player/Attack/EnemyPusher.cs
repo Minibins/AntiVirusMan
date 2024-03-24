ad9a198a95bc7e4354078bcf0139716570a7b722
AntonXCM <antony.prykhodchenko@gmail.com>
2024-03-24 13:24:17 +0200

    Музыка, починка багов, ченжлог в меню

--- Assets/Scripts/Persons/Player/Attack/EnemyPusher.cs
+++ Assets/Scripts/Persons/Player/Attack/EnemyPusher.cs
@@ -7,12 +7,14 @@ using UnityEngine;
 public class EnemyPusher : MonoBehaviour, IAttackProjectileModule
 {
     [SerializeField] float Power;
+    [SerializeField] LayerMask layer;
     public void Attack(IDamageble target,Collider2D collision)
     {
         MoveBase move;
         if(collision.gameObject.TryGetComponent<MoveBase>(out move))
         {
-            move.Rigidbody.velocity = new( MathA.OneOrNegativeOne(GetComponent<SpriteRenderer>().flipX)*Power*(1+Convert.ToInt16(collision.IsTouchingLayers(4))),move.Rigidbody.velocity.y);
+            print(collision.IsTouchingLayers(layer));
+            move.Rigidbody.velocity = new( MathA.OneOrNegativeOne(GetComponent<SpriteRenderer>().flipX)*Power*(1+Convert.ToInt16(collision.IsTouchingLayers(layer))),move.Rigidbody.velocity.y);
         }
     }
 }
