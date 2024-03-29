cbc6fc032a883cfce001529c53dd127fc0b0f055
AntonXCM <antony.prykhodchenko@gmail.com>
2024-03-29 14:07:09 +0200

    Жвачку можно таскать

--- Assets/Scripts/Upgrade/ButtonsBehavior/DragUpgrade.cs
+++ Assets/Scripts/Upgrade/ButtonsBehavior/DragUpgrade.cs
@@ -9,6 +9,8 @@ public class DragUpgrade : Upgrade
         base.OnTake();
         foreach(TurretLikeUpgrade turret in GameObject.FindObjectsOfType<TurretLikeUpgrade>())
             turret.AddComponent<DRAG>();
+        foreach(DraggableAsTurret draggable in GameObject.FindObjectsOfType<DraggableAsTurret>()) 
+            draggable.AddComponent<DRAG>();
         GameObject.FindObjectOfType<PChealth>().AddComponent<DRAG>();
         FindObjectOfType<InstantiateWall>().canmove = true;
     }
--- Assets/Scripts/Upgrade/DraggableAsTurret.cs
+++ Assets/Scripts/Upgrade/DraggableAsTurret.cs
@@ -1,18 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
 public class DraggableAsTurret : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if(LevelUP.Items[1].IsTaken)
+        {
+            gameObject.AddComponent<DRAG>();
+        }
     }
 }
