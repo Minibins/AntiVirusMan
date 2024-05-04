c351e621115249621d8e49fee902d75cefbec485
Maksym <127485465+Minibins@users.noreply.github.com>
2024-05-04 17:54:30 +0200

    Add Eggs 2024 to database

--- Assets/Scripts/UI/Eggs/EggItem.cs
+++ Assets/Scripts/UI/Eggs/EggItem.cs
@@ -7,9 +7,11 @@ public class EggItem : EggStatsItem
     [SerializeField] private String _text;
     [SerializeField] private Image _mainSprite;
     [SerializeField] private Text _mainText;
-
+    
     public override void SetInfromation()
     {
+        _itemSprite = GetComponent<Image>();
+        _itemSprite.color = _count > 0 ? Color.white : Color.black;
         _mainSprite.color = _count > 0 ? Color.white : Color.black;
         _mainSprite.sprite = _itemSprite.sprite;
         _mainText.text = _text;
--- Assets/Scripts/UI/Eggs/EggStatsItem.cs
+++ Assets/Scripts/UI/Eggs/EggStatsItem.cs
@@ -5,23 +5,25 @@ using UnityEngine.UI;
 public class EggStatsItem : MonoBehaviour
 {
     [SerializeField] private string _key;
-    protected int _count;
+    public int _count;
     protected Image _itemSprite;
     
-    private void Start()
+    public void Start()
     {
         UpdateStats();
     }
 
     public void UpdateStats()
     {
-        _count = PlayerPrefs.GetInt(_key, 0);
-        _itemSprite = GetComponent<Image>();
-        _itemSprite.color = _count > 0 ? Color.white : Color.black;
+        _count = PlayerPrefs.GetInt(_key, 0); 
+        SetInfromation();
     }
     
     public virtual void SetInfromation()
     {
+        _itemSprite = transform.GetChild(0).gameObject.GetComponent<Image>();
+        _itemSprite.color = _count > 0 ? Color.white : Color.black;
+        
         transform.GetChild(1).gameObject.GetComponent<Text>().text = _count.ToString();
     }
 }
